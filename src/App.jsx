34e7edd4e94f4b93f2823c8a9d06ea1dbd264eb9
tdora28 <tokaidora88@gmail.com>
2023-11-09 15:53:00 +0200

    Add palindrome checker function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-// import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
+  const [userInput, setUserInput] = useState('');
+
+  const checkPalindrome = (input) => {
+    let text = cleanInput(input);
+    if (text.length === 0) return '';
+    let falseCounter = 0;
+    for (let i = 0; i < Math.ceil(text.length / 2); i++) {
+      let j = text.length - 1 - i;
+      if (text[i] !== text[j]) {
+        falseCounter++;
+        break;
+      }
+    }
+    return falseCounter > 0 ? 'not palindrome' : 'palindrome';
+  };
+
+  const cleanInput = (susInput) => {
+    const cleaned = susInput.replace(/\s/g, '').toLowerCase();
+    return cleaned;
+  };
+
   return (
     <>
-      <h1>hello</h1>
+      <h1>Palindrome Checker</h1>
+      <input type='text' id='userInput' value={userInput} placeholder='Type here...' onChange={(e) => setUserInput(e.target.value)} />
+      {checkPalindrome(userInput)}
     </>
   );
 }
